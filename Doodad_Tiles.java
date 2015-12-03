a41d6fbada6dce3b8d9e2424100e7e56b0438a6f
Cameron J. Wilson <cameron_wilson@student.sscps.org>
2015-12-03 09:16:26 -0500

    Update 1.0.0
    Added Game RTS-Test
    Added Main World, Actors, and Actor Subclasses

---
+++ Doodad_Tiles.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Doodad_Tiles here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Doodad_Tiles extends Objects
+{
+    /**
+     * Act - do whatever the Doodad_Tiles wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
