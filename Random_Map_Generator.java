60b930e3971a5d3e228039b5ec8fd8d6dd120f8c
Cameron J. Wilson <cameron_wilson@student.sscps.org>
2015-12-08 11:36:59 -0500

    update 1.0.6
    -ADDED Randomly Generated World with a Randomly placed building
    -Working on creating natural border with trees

--- Random_Map_Generator.java
+++ Random_Map_Generator.java
@@ -15,8 +15,14 @@ public class Random_Map_Generator extends Map_Tiles
     int tile_type = 0;
     int w = 0;
     int h = 0;
-    int xZ = 0;
-    int yZ = 0;
+    int xZ = -16;
+    int yZ = -16;
+    boolean dirty_flag_rmg = false;
+    boolean dfT = false;
+    boolean System_RPTH_failed_Check = false;
+    boolean System_Render_Complete = false;
+    int check = 0;
+    int Building_Count = 1;
     public void act() 
     {
          background Tile = (background)this.getWorld();
@@ -27,47 +33,81 @@ public class Random_Map_Generator extends Map_Tiles
              if (w <= 79)
              {
              tile_type = getRandomNumberRange(1, 10);
-             
-             
-              
              w = (w + 1) % 80;
              xZ = (xZ + 16);
              System.out.println("yZ: " + yZ + " xZ: " + xZ);
              if (xZ >= 1264){ 
-                 xZ = 0; 
+                 yZ = yZ + 16;
+                 xZ = 0;
+                 
                  System.out.println("yZ: " + yZ + " xZ: " + xZ);
                 }
              //tileGenTestRange = (tileGenTestRange + 1) % 10;
             } 
-            if (h <= 39)
-            {
-            tile_type = getRandomNumberRange(1, 10);
-            h = (h + 1) % 40;
-            yZ = (yZ + 16);
-            System.out.println("yZ: " + yZ + " xZ: " + xZ);
             
-            if (yZ >= 624){
-                yZ = 0; 
-                System.out.println("yZ: " + yZ + " xZ: " + xZ);
-            }
-        }
-            if (tile_type <= 1)
-            {
-                Tile.addObject(new Forest(), xZ, yZ);
-                tile_n = (tile_n + 1);
-                System.out.println("tile total: " + tile_n);
+                if ((tile_type <= 1) && ((xZ <= 0) || (yZ <= 0)) || (xZ >= 1024 && yZ >= 608))
+                {
                 
-            }
+                    Tile.addObject(new Forest(), xZ, yZ);
+                
+                    tile_n = (tile_n + 1);
+                
+                    System.out.println("tile total: " + tile_n);
+                
+                } else if ((tile_type <= 1) && (!(xZ <= 64 && yZ <= 64) || ( xZ >= 1024 && yZ >= 624)))
+                { 
+                
+                    Tile.addObject(new Plain(), xZ, yZ);
+                
+                    tile_n = (tile_n + 1);
+                
+                    System.out.println("tile total: " + tile_n);
+               }
             
-            if (tile_type >= 2)
-            { 
-                Tile.addObject(new Plain(), xZ, yZ);
-                tile_n = (tile_n + 1);
-                System.out.println("tile total: " + tile_n);
-            }
-     } else if (tile_n == 3081){
-         System.out.println("Render Finished!");
+               if ((tile_type >= 2) && (!(xZ <= 64 && yZ <= 64) || ( xZ >= 1024 && yZ >= 624)))
+               { 
+                
+                   Tile.addObject(new Plain(), xZ, yZ);
+                
+                   tile_n = (tile_n + 1);
+                
+                   System.out.println("tile total: " + tile_n);
+               
+                }
+     } else if (tile_n >= 3081){
+         if (!(dirty_flag_rmg)){
+         System.out.println("Render Initialization Finished!");
+         
+         dirty_flag_rmg = true;
         }
+      if ((dirty_flag_rmg) && (!(dfT))){
+            tile_type = getRandomNumberRange(1, 10);
+            xZ = getRandomNumberRange(256, 1008);
+            yZ = getRandomNumberRange(64, 560);
+        if ((tile_type == 7) && (xZ <= 1008) && (xZ >= 256) && (yZ <= 560) && (yZ >= 64))
+            {
+               Tile.addObject(new TH_T1(), xZ, yZ);               
+               Building_Count = 2;
+               // when building count = 2, it just means theres 1 building;
+            
+        if (Building_Count == 2){
+                System.out.println("System Render Buildings Done!");
+                dfT = true;
+        } else if ((Building_Count == 1) && (!(System_RPTH_failed_Check)) && (!(dfT))){
+            
+            System.out.println("System.Render.Player.Town_Hall Failed lotto");
+            System.out.println("Restarting System.Render.Town_Hall...");
+            System_RPTH_failed_Check = true;
+        } 
+    }
+    } 
+    if ((dfT) && (!(System_Render_Complete))) { 
+        System.out.println("System.Render.Player.Town_Hall Finished");
+        System_Render_Complete = true;
+    
+    }
+}
+        
     }    
 }
 }
--- Trade_Merchants.java
+++ Trade_Merchants.java
@@ -1,19 +1,19 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Trade_Merchants here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Trade_Merchants extends Buildings
-{
-    /**
-     * Act - do whatever the Trade_Merchants wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
-}
+// import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+// 
+// /**
+//  * Write a description of class Trade_Merchants here.
+//  * 
+//  * @author (your name) 
+//  * @version (a version number or a date)
+//  */
+// public class Trade_Merchants extends Buildings
+// {
+//     /**
+//      * Act - do whatever the Trade_Merchants wants to do. This method is called whenever
+//      * the 'Act' or 'Run' button gets pressed in the environment.
+//      */
+//     public void act() 
+//     {
+//         // Add your action code here.
+//     }    
+// }
